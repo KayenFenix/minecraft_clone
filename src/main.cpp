eaf342a5a05a0d374372742b883d7e2c751464a8
KayenFenix <mikoshabbaz17@gmail.com>
2024-01-27 20:37:25 +0600

    added glad

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,47 @@
+#include <glad/glad.h>
 #include <glfw/glfw3.h>
 #include <iostream>
 
 int main()
 {
+    if (!glfwInit())
+    {
+        std::cout << "Failed to init glfw." << std::endl;
+        return -1;
+    }
+
+    GLFWwindow *pWindow = glfwCreateWindow(
+        800, 600,
+        "OpenGL Window",
+        nullptr, nullptr);
+
+    if (pWindow == nullptr)
+    {
+        std::cout << "Failed to create glfw-window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(pWindow);
+    if (!gladLoadGL())
+    {
+        std::cout << "Cannot load glad." << std::endl;
+        return -1;
+    }
+
+    glfwWindowHint(GL_MAJOR_VERSION, 4);
+    glfwWindowHint(GL_MINOR_VERSION, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    glClearColor(1, 1, 0, 1);
+    while (!glfwWindowShouldClose(pWindow))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(pWindow);
+        glfwPollEvents();
+    }
+
+    glfwSetWindowShouldClose(pWindow, false);
+    glfwTerminate();
     std::cout << "Hello!\n";
     return 0;
 }
